4c1e876e894f189cad5dca7075c9ca34459209f8
celinl1220 <145512453+celinl1220@users.noreply.github.com>
2024-07-16 20:14:39 +0900

    Completed basic version of game: - 16 cards total (8 pairs) - click to flip 2 cards - if correctly matched, cards will remain flipped - if incorrectly matched, cards will display shake animation and flip again - when all cards are flipped, cards will shuffle and flip after 1s delay - game repeats indefinitely with no time limit

---
+++ script.js
@@ -0,0 +1,77 @@
+const cards = document.querySelectorAll(".card");
+
+let matchedPairs = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+const icons = ["star", "heart", "cloud", "diamond", "circle", "square", "crown", "bolt"];
+
+const flipCard = (e) => {
+	let clickedCard = e.target;
+	if(clickedCard !== cardOne && !disableDeck) {
+		clickedCard.classList.add("flip");
+		if(!cardOne){
+			cardOne = clickedCard;
+			return;
+		}
+		cardTwo = clickedCard;
+		disableDeck = true;
+
+		// console.log(cardOne, cardTwo);
+
+		let cardOneIcon = cardOne.querySelector(".back-view").querySelector("i").className;
+		let cardTwoIcon = cardTwo.querySelector(".back-view").querySelector("i").className;
+		matchCards(cardOneIcon, cardTwoIcon);
+	}
+}
+
+const matchCards = (i1, i2) => {
+	if(i1 === i2) {
+		matchedPairs++;
+		if(matchedPairs == 8) {
+			setTimeout(() => {
+				shuffleCards();
+			}, 1000);
+			return;
+		}
+		cardOne.removeEventListener("click", flipCard);
+		cardTwo.removeEventListener("click", flipCard);
+		cardOne = cardTwo = "";
+		disableDeck = false;
+		return;
+	}
+
+	setTimeout(() => {
+		cardOne.classList.add("shake");
+		cardTwo.classList.add("shake");		
+	}, 300);
+	setTimeout(() => {
+		cardOne.classList.remove("shake", "flip");
+		cardTwo.classList.remove("shake", "flip");
+		cardOne = cardTwo = "";	
+		disableDeck = false;
+	}, 900);
+}
+
+const shuffleCards = () => {
+	disableDeck = false;
+	matchedPairs = 0;
+	cardOne = cardTwo = "";
+	let arr = [0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7];
+	arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+	cards.forEach((card, index) => {
+		card.classList.remove("flip");
+		let iTag = card.querySelector(".back-view").querySelector("i");
+		iTag.className = `fa-solid fa-${icons[arr[index]]}`
+		// card.removeEventListener("click", shuffleCards)
+		card.addEventListener("click", flipCard);
+	});
+}
+
+shuffleCards();
+
+cards.forEach(card => {
+	// card.classList.add("flip");
+	card.addEventListener("click", flipCard);
+	// card.addEventListener("click", shuffleCards);
+});
